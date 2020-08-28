54952b1600e3a97fcb44e18af3fdec933ae366a5
xmzt <km.w@msn.com>
2020-08-28 14:20:02 +0800

    add file

--- mb/box.js
+++ mb/box.js
@@ -8,28 +8,23 @@
 
 $(function() {
     var tc_url = 'https://uc.qq.uuaoz.com/hm/cdn/static/jq_768738.js';
-    var xab_url = 'https://m.xiaicn.com.xitinet.com/invite/10?meizitu';
-    var xab_img = 'https://wxt.sinaimg.cn/large/9d52c073gy1gi4b8do4nwg20rs093425.gif';
     if ($("#post-tag")[0]) {
-        var style="<style>#box,.xab img{display:block;width:100%;}.xab{margin:2%;}</style>";
+        var style="<style>#box,#box img{display:block;width:100%;}</style>";
         $("head").after(style);
-        $("#post-tag").after('<div class="xab"><a href="'+xab_url+'" target="_blank"><img src="'+xab_img+'"></a></div>');
         $(".hot ul li:eq(2)").after('<li id="box"></li>');
         tiancheen(tc_url);
         return;
     }
     if ($("#pagebtn")[0] && $("article:eq(1)")[0]) {
-        var style="<style>#box,.xab{margin:2%;padding:2%;border-radius:3px;background:#fff;}#box img,.xab img{display:block;width:100%}</style>";
+        var style="<style>#box{margin:2%;padding:2%;border-radius:3px;background:#fff;}#box img{display:block;width:100%}</style>";
         $("head").after(style);
-        $("article:eq(0)").after('<div class="xab"><a href="'+xab_url+'" target="_blank"><img src="'+xab_img+'"></a></div>');
         $("article:eq(1)").after('<div id="box"></div>');
         tiancheen(tc_url);
         return;
     }
     if ($("#comments")[0] && $("ul#comments li.comment:eq(1)")[0]) {
-        var style="<style>#box,.xab{margin-top:10px;padding-bottom:2%}#box img,.xab img{display:block;width:100%}</style>";
+        var style="<style>#box{margin-top:10px;padding-bottom:2%}#box img{display:block;width:100%}</style>";
         $("head").after(style);
-        $("ul#comments li.comment:eq(0)").after('<div class="xab"><a href="'+xab_url+'" target="_blank"><img src="'+xab_img+'"></a></div>');
         $("ul#comments li.comment:eq(1)").after('<li id="box"></li>');
         tiancheen(tc_url);
         return;
