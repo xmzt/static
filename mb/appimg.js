c3269f420ded2fc6cc965517ab20ab7d510b4a44
xmzt <km.w@msn.com>
2020-08-31 00:27:40 +0800

    add file

--- mb/appimg.js
+++ mb/appimg.js
@@ -4,15 +4,15 @@
         var n = Math.floor(Math.random()*8)+1;
         var imgurl = 'https://cdn.jsdelivr.net/gh/xmzt/static/mb/appimg/'+n+'.gif';
         if ($("#post-tag")[0]) {
-            $("#post-tag").after('<div style="margin:2%;margin-top:1.4em"><h3 class="block-line"><span>下载APP</span></h3><a href="https://m.mzitu.com/app/" target="_blank"><img style="width:100%;display:block;" src="'+imgurl+'" alt="下载妹子图APP"></a></div>');
+            $("#post-tag").after('<div style="margin:2%;margin-top:1.4em"><h3 class="block-line"><span>妹子图官方APP</span></h3><a href="https://m.mzitu.com/app/" target="_blank"><img style="width:100%;display:block;" src="'+imgurl+'" alt="下载妹子图APP"></a></div>');
             return;
         }
         if ($("#pagebtn")[0] && $("article:eq(0)")[0]) {
-            $("article:eq(0)").after('<div style="margin:2%;padding:2%;border-radius:3px;background:#fff;"><span style="display:block;color:#ff88af;font-size:14px;font-weight:bold;margin-bottom:2%;">下载妹子图APP</span><a href="https://m.mzitu.com/app/" target="_blank"><img style="width:100%;display:block;" src="'+imgurl+'" alt="下载妹子图APP"></a></div>');
+            $("article:eq(0)").after('<div style="margin:2%;padding:2%;border-radius:3px;background:#fff;"><span style="display:block;color:#ff88af;font-size:14px;font-weight:bold;margin-bottom:2%;">妹子图官方APP</span><a href="https://m.mzitu.com/app/" target="_blank"><img style="width:100%;display:block;" src="'+imgurl+'" alt="下载妹子图APP"></a></div>');
             return;
         }
         if ($("#comments")[0]) {
-            $("ul#comments li.comment:eq(0)").after('<div style="margin-top:10px;padding-bottom:2%"><span style="display:block;color:#ff88af;font-size:12px;margin-bottom:2%;">下载妹子图APP</span><a href="https://m.mzitu.com/app/" target="_blank"><img style="width:100%;display:block;" src="'+imgurl+'" alt="下载妹子图APP"></a></div>');
+            $("ul#comments li.comment:eq(0)").after('<div style="margin-top:10px;padding-bottom:2%"><span style="display:block;color:#ff88af;font-size:12px;margin-bottom:2%;">妹子图官方APP</span><a href="https://m.mzitu.com/app/" target="_blank"><img style="width:100%;display:block;" src="'+imgurl+'" alt="下载妹子图APP"></a></div>');
             return;
         }
     }
