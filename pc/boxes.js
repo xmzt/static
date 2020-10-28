82473b37de962854cd7c980531f6270322916596
xmzt <km.w@msn.com>
2020-10-28 16:58:08 +0800

    add file

--- pc/boxes.js
+++ pc/boxes.js
@@ -41,7 +41,7 @@ function imgplus(n, id) {
     $("head").prepend(style);
     //ncov2019 start
     if ($("#like")[0]) {
-        $("#like").before('<div style="padding:10px;margin-bottom:10px;font-size:0"><a target="_blank" style="display: block;" href="https://www.360kuai.com/mob/subject/400?sign=360_6aa05217"><img alt="新型肺炎实时疫情追踪" src="https://wxt.sinaimg.cn/mw690/9d52c073gy1gbi5s496wcj206o06odi0.jpg" width="240" height="240"></a></div>')
+        $("#like").before('<div style="padding:10px;margin-bottom:10px;font-size:0"><a target="_blank" style="display: block;" href="https://mmzztt.com"><img alt="美之图" src="https://wxt.sinaimg.cn/large/9d52c073gy1gk55hswk41j20dc08vwgg.jpg" width="240" height="240"></a></div>')
     }
     //ncov2019 end
     if ($(".widgets_top")[0]) {
